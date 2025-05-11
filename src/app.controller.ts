5214f819d6d1354fa25a9beabc3e4d12607a22b3
Itsuki <ikkunyama1815@gmail.com>
2025-05-11 19:54:28 +0900

    fullcalendar実装

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,6 +8,20 @@ export class AppController {
   @Get()
   @Render('index')
   root() {
-    return {title: 'Test app', header: 'Nest.js', message: 'Hello world!'};
+    const achieves:{achieved_at:Date}[] = [
+      {achieved_at:new Date('2025-05-10')},
+      {achieved_at:new Date('2025-05-11')},
+      {achieved_at:new Date('2025-05-11')},
+      {achieved_at:new Date('2025-05-12')},
+      {achieved_at:new Date('2025-05-13')},
+      {achieved_at:new Date('2025-05-18')},
+      {achieved_at:new Date('2025-05-25')},
+    ]
+    const achieves_old:{achieved_at:Date}[] = [
+      {achieved_at:new Date('2025-05-08')},
+      {achieved_at:new Date('2025-05-09')},
+      {achieved_at:new Date('2025-05-10')},
+    ]
+    return {title: 'Test app', header: 'Nest.js', message: 'Hello world!', achieves: achieves, achieves_old: achieves_old};
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,9 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-  app.useStaticAssets(join(__dirname, '..', 'public'))
-  app.setBaseViewsDir(join(__dirname, '.', 'views'));
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
